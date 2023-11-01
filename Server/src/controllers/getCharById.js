c223fc0eb5171fa09ce2b38d5c43df6a0ba1943e
AleSaturno <alesaturno64@gmail.com>
2023-11-01 16:33:14 -0300

    Create getCharById.js

---
+++ Server/src/controllers/getCharById.js
@@ -0,0 +1,36 @@
+const axios = require("axios");
+const URL = "https://db.ygoprodeck.com/api/v7/cardinfo.php";
+
+const getCharById = async(req , res) =>{
+    try {
+        const {id} = req.params;
+
+        const response = await axios(`${URL}/${id}`);
+        const {name,type,frameType,desc,race,archetype,card_sets, card_images} = response.data;
+
+        if(name){
+            const character = {
+                id,
+                name,
+                type,
+                frameType,
+                desc,
+                race,
+                archetype,
+                card_sets,
+                card_images
+            };
+            return res.status(200).json(character);
+        }
+
+
+        return res.status(404).send("Not Found");
+
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
+
+module.exports = {
+    getCharById
+};
