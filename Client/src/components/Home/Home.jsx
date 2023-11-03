61044133108ece8a78360f14ce816893e770c907
Alejandro Saturno <alesaturno64@gmail.com>
2023-11-03 13:56:42 -0300

    Update Home.jsx

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -1,33 +1,31 @@
-import Card from "../CardYugi/Card"
+import Card from "../CardYugi/Card";
 
+const Home = ({ deck }) => {
+  if (!deck || !Array.isArray(deck)) {
+    return <div>No se ha proporcionado un mazo válido.</div>;
+}
 
-const Home = ({char}) =>{
-    return(
-        <>
-            {
-                char.map(({id, name,type,frameType,desc,race,archetype,card_sets, card_images})=>{
-                    
-                    return(
-                        <Card
-                        
-                            key={id}
-                            id={id}
-                            name={name}
-                            type={type}
-                            frameType={frameType}
-                            descriptions={desc}
-                            race={race}
-                            archetype={archetype}
-                            card_sets={card_sets}
-                            card_images={card_images}  
-                        
-                        />
-                    );
-                })
-            };
-        
-        </>
-    );
+  return (
+    <div>
+      {deck.map(({ id, name, type, frameType, desc, race, archetype, card_sets, card_images }) => {
+        return (
+          <Card
+            key={id}
+            id={id}
+            name={name}
+            type={type}
+            frameType={frameType}
+            descriptions={desc}
+            race={race}
+            archetype={archetype}
+            card_sets={card_sets}
+            card_images={card_images}
+          />
+        );
+      })}
+    </div>
+  );
+  
 };
 
 export default Home;
